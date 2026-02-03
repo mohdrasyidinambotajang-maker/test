f59b258f5565f8de4850dca88c84236725f05069
mohdrasyidinambotajang-maker <mohdrasyidinambotajang@gmail.com>
2026-02-03 13:03:42 +0800

    first commit

---
+++ py/01_variables_data_types.py
@@ -0,0 +1,30 @@
+name = "Bob" #String
+age = 10     # Integer
+weight = 20.5 # Float
+is_female = False # Boolean
+her_name = "Alice" #String
+random_number = None # Integer
+tapir = "animal"
+
+
+print(name)
+print(type(name))
+print(her_name)
+print(type(random_number))
+
+
+x = 10
+y = 3
+
+
+
+print(x + y)  # Addition
+print(x - y)  # Subtraction
+print(x * y)  # Multiplication
+print(x / y)  # Division
+print(x // y) # Floor Division
+print(x % y)  # Modulus
+print(x ** y) # Exponentiationprint(type(a
+
+
+
---
+++ py/pybootcamp.py
@@ -0,0 +1,8 @@
+name = "Bob" #String
+age = 10     # Integer
+weight = 20.5 # Float
+is_female = False # Boolean
+
+print(name)
+print(type(name))
+
